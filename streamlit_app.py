a2d43b43ff9a66d1a220224af3a3d84f11a3fbf3
angga7353 <angga.fpriyanto@gmail.com>
2025-05-21 11:55:21 +0700

    Update:script3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,15 @@
 import streamlit as st
-# from ultralytics import YOLO
-from PIL import Image
-import numpy as np
 import importlib
 
-Yolo = importlib.import_module("ultralytics.models.yolo.model").Yolo
-
-model = Yolo("best.pt")
+YOLO = importlib.import_module("ultralytics.models.yolo.model").YOLO
 
 def app():
-    st.title("AI SEE YOU")
-
-    file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
-    if file is not None:
-        img = Image.open(file).convert("RGB")
-        st.image(img, caption="Uploaded Image", use_column_width=True)
-
-        img_np = np.array(img)
-
-        results = model.predict(source=img_np, conf=0.25, save=False)
-        
-        # Process results list
-        for result in results:
-            boxes = result.boxes  # Boxes object for bounding box outputs
-            masks = result.masks  # Masks object for segmentation masks outputs
-            keypoints = result.keypoints  # Keypoints object for pose outputs
-            probs = result.probs  # Probs object for classification outputs
-            obb = result.obb  # Oriented boxes object for OBB outputs
-            result.show()  # display to screen
-            result.save(filename="result.jpg")  # save to disk
+    st.title("YOLO Test")
+    try:
+        model = YOLO("yolov8n.pt")  # atau "best.pt" jika sudah upload
+        st.success("Model loaded successfully!")
+    except Exception as e:
+        st.error(f"Error loading model: {e}")
 
 if __name__ == "__main__":
     app()
