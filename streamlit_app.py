f35c936d75d28dbdc50a372aba89584e2d5465f9
angga7353 <angga.fpriyanto@gmail.com>
2025-05-21 15:46:41 +0700

    Update:Script3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,54 @@
 import streamlit as st
-import importlib
+from ultralytics import YOLO
+from PIL import Image
+import numpy as np
+# import importlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
-YOLO = importlib.import_module("ultralytics.models.yolo.model").YOLO
+# Yolo = importlib.import_module("ultralytics.models.yolo.model").Yolo
+
+model = YOLO("best.pt")
+
+label_map = {
+    0: "scissors",
+    1: "unidentified",
+    2: "knife",
+    3: "cutter",
+    4: "swiss knife",
+}
 
 def app():
-    st.title("YOLO Test")
-    try:
-        model = YOLO("yolov8n.pt")  # atau "best.pt" jika sudah upload
-        st.success("Model loaded successfully!")
-    except Exception as e:
-        st.error(f"Error loading model: {e}")
+    st.title("AI SEE YOU")
+
+    file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+    if file is not None:
+        img = Image.open(file).convert("RGB")
+        st.image(img, caption="Uploaded Image", use_column_width=True)
+        
+        results = model.predict(source=img, conf=0.25, save=False)
+        
+        if results:
+            st.write("Results:")
+            for result in results:
+                # st.write(f"Boxes: {result.boxes}")
+                if result.boxes.cls.numel() > 0:
+                    # st.write(f"Boxes (xyxy): {result.boxes.xyxy}")
+                    # st.write(f"CLS (xywh): {result.boxes.cls[0]}")
+
+                    fig, ax = plt.subplots()
+                    ax.imshow(img)
+
+                    x1, y1, x2, y2 = result.boxes.xyxy[0]
+                    rect = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=1, edgecolor="r", facecolor="none")
+                    ax.add_patch(rect)
+
+                    ax.text(x1, y1, f"{label_map[int(result.boxes.cls[0])]} {result.boxes.conf[0]:.2f}", fontsize=12, color="white", bbox=dict(facecolor="red", alpha=0.5))
+                    ax.axis("off")
+
+                    st.pyplot(fig)
+                else:
+                    st.write("No objects detected.")
 
 if __name__ == "__main__":
     app()
